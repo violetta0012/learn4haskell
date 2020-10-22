e0217d992b6614802eee7f23834f572cf5bd2f04
Виолетта Герасимова <violettagerasimova@MacBook-Air-Violetta.local>
2020-10-22 12:53:06 +0300

    completed task 5

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -513,8 +513,10 @@ True
 >>> isThird42 [42, 42, 0, 42]
 False
 -}
-isThird42 = error "isThird42: Not implemented!"
 
+isThird42 :: [Int] -> Bool
+isThird42 (_ : _ : 42 : _) = True
+isThird42 _ = False
 
 {- |
 =🛡= Recursion
